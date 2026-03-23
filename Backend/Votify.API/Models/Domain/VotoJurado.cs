d518c63dce38843ddf676633f0976639f2d7b202
Brad Owen Valladares Quispe <BOVALQUI@upv.edu.es>
2026-03-23 23:18:10 +0100

    implemento votoJurado(simple)

--- Backend/Votify.API/Models/Domain/VotoJurado.cs
+++ Backend/Votify.API/Models/Domain/VotoJurado.cs
@@ -6,7 +6,7 @@ public class VotoJurado : Voto
 {// si el metodo ya estaba en voto poner override;
     public  string ObtenerTipoVotante() => "JURADO";
 
-    public  float CalcularPuntuacionFinal(float peso) 
+    public override float CalcularPuntuacionFinal(float peso) 
     {
         return Valor * peso;
     }
