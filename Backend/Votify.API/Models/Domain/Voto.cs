4e064dbec8e087c3c72b090ace0e5ad72e7b3e90
Javier Pereperez Cardona <JPERCAR@upv.edu.es>
2026-04-02 21:21:12 +0200

    Cambios en las clases

---
+++ Backend/Votify.API/Models/Domain/Voto.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+
+namespace Votify.API.Models.Domain
+{
+    public class Voto : BaseModel
+    {
+        [PrimaryKey("id", false)]
+        public string Id { get; set; }
+
+        [Column("valor")]
+        public float valor { get; set; }
+
+        [Column("comentario")]
+        public String comentario { get; set; }
+
+        [Column("urlaudio")]
+        public String urlaudio { get; set; }
+
+        [Column("ipdispositivo")]
+        public float ipddispositivo { get; set; }
+
+        [Column("fechavoto")]
+        public  DateTime fechavoto { get; set; }
+
+        [Column("idproyecto")]
+        public int idproyecto { get; set; }
+
+        [Column("idevaluador")]
+        public int idevaluador { get; set; }
+
+        [Column("idcriterio")]
+        public int idcriterio { get; set; }
+
+        [Column("idcategoria")]
+        public int idcategoria { get; set; }
+
+        public Voto() { }
+
+        public Voto (string id, float valor,
+            string comentario, string urlaudio,
+            float ipddispositivo, DateTime fechavoto,
+            int idproyecto, int idevaluador,
+            int idcriterio, int idcategoria)
+        {
+            Id = id;
+            this.valor = valor;
+            this.comentario = comentario;
+            this.urlaudio = urlaudio;
+            this.ipddispositivo = ipddispositivo;
+            this.fechavoto = fechavoto;
+            this.idproyecto = idproyecto;
+            this.idevaluador = idevaluador;
+            this.idcriterio = idcriterio;
+            this.idcategoria = idcategoria;
+        }
+    }
+}
---
+++ Backend/Votify.API/Services/ComentarioCualitativoService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Diagnostics;
+using Supabase;
+using Supabase.Interfaces;
+using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks;
+using Votify.API.Models.Domain;
+using Votify.API.Models.DTOs;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Votify.API.Services;
+
+public class ComentarioCualitativoService(Client supabase)
+{
+
+    public int Id { get; set; }
+    public int IdCriterio { get; set; }
+    public required string Comentario { get; set; }
+
+
+    private readonly Client _supabase = supabase;
+
+    [HttpPost]
+
+    public async Task<string?> GetComentarioVoto(string votoId)
+    {
+        var response = await _supabase
+            .From<Voto>()
+            .Where(x => x.Id == votoId)
+            .Get();
+
+        string? comentario = response.Models.FirstOrDefault()?.comentario ;
+        if (comentario != null) return comentario;
+        return "null";
+    }
+
+    public string GuardarComentario(int id, int idCriterio, string comentario, string votoId)
+    {
+        this.Id = id;
+        this.IdCriterio = idCriterio;
+        this.Comentario = GetComentarioVoto(votoId).ToString();
+
+        return ObtenerComentario();
+
+    }
+
+    public string ObtenerComentario()
+    {
+        return "Id: "+Id+", IdCriterio: "+IdCriterio+", Texto"+Comentario;
+    }
+
+}
+
