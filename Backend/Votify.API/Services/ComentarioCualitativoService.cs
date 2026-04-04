1cb19abab61f8238930075fe24cd9f9f6572a79e
Javier Pereperez Cardona <JPERCAR@upv.edu.es>
2026-04-04 17:22:39 +0200

    cambios varios

--- Backend/Votify.API/Services/ComentarioCualitativoService.cs
+++ Backend/Votify.API/Services/ComentarioCualitativoService.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Diagnostics;
+﻿            using Microsoft.AspNetCore.Mvc;
 using Supabase;
 using Supabase.Interfaces;
 using Supabase.Postgrest.Attributes;
